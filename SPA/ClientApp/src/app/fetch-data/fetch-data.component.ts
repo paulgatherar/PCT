4e59fefb3ba711fb8898efbe4ea00669fd235709
Paul Gatherar <paul.gatherar@sage.com>
2021-10-26 14:04:51 +0100

    Fetches people data from API and displays data on the UI.

--- SPA/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ SPA/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,17 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public people: Person[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<Person[]>(baseUrl + 'api/people').subscribe(result => {
+      this.people = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Person {
+  id: number;
+  name: string;
+  age: number;
 }
