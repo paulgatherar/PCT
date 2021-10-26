b0c2496e5b9a81f8a2dde242a2ecb639eade3b11
Paul Gatherar <paul.gatherar@sage.com>
2021-10-26 15:05:08 +0100

    Removes unnecessary components.

--- SPA/ClientApp/src/app/people/people.component.ts
+++ SPA/ClientApp/src/app/people/people.component.ts
@@ -2,10 +2,10 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  selector: 'app-people',
+  templateUrl: './people.component.html'
 })
-export class FetchDataComponent implements OnInit {
+export class PeopleComponent implements OnInit {
   public people: Person[];
   public sortByAge: boolean = false;
   public reverseNames: boolean = false;
