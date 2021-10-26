d3e1cc2e418024f4e88a22296131c1265d5f4195
Paul Gatherar <paul.gatherar@sage.com>
2021-10-26 14:02:28 +0100

    Adds `PeopleController` and `Person` model.

---
+++ SPA/Controllers/PeopleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace SPA.Controllers
+{
+    [ApiController]
+    [Route("api/people")]
+    public class PeopleController : ControllerBase
+    {
+        const string url = "https://techtestpersonapi.azurewebsites.net/api/GETPersonsTechTestAPI?code=Z5Dm297Ijn9weSo75EVtsJHN9HoVE0fgJt8zIGXWV4ZOOCGNpaYBtw==";
+
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public PeopleController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Person>> Get()
+        {
+            var httpClient = this.httpClientFactory.CreateClient();
+
+            var httpResponseMessage = await httpClient.GetAsync(url);
+
+            // Can throw an exception. Handled by middleware.
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<IEnumerable<Person>>(content);
+        }
+    }
+}
--- SPA/Startup.cs
+++ SPA/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace SPA
 {
@@ -26,6 +27,8 @@ namespace SPA
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
